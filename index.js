788fba0877091f098a6e77485e792ab14515ee55
RomaelZero <73689142+RomaelZero@users.noreply.github.com>
2024-03-18 15:54:19 +0530

    Update index.js for the secondtime

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ function singleRoll(){
 console.log("The putput for a single dice roll is : "+ singleRoll());
 
 // Multiple Roll in Array form as single elements
-const outputForDice1 = [];
+let outputForDice1 = [];
 
 function multiRollDiceArray(n = 2, m = 6) {
+    outputForDice1 = [];
     if (typeof n === 'string') {
         throw new Error("Number of rolls must be a positive integer. You have passed a string.");
     }
@@ -28,18 +29,19 @@ function multiRollDiceArray(n = 2, m = 6) {
 
 //The output of the rice will be available as single elments of an array. 
 //i.e Each element of the array will be the number obtained from rolling the dice
-console.log(multiRollDiceArray()); //<<== the function to get diceArray with objects as single elements.
+//console.log(multiRollDiceArray()); //<<== the function to get diceArray with objects as single elements.
 
 
 
 
 
 // Multiple Roll in Array form as objects
-const outputForDice2 = [];
+let outputForDice2 = [];
 
 
 
 function multiRollDiceArrayObject(n = 2, m = 6) {
+    outputForDice2 = [];
     if (typeof n === 'string') {
         throw new Error("Number of rolls must be a positive integer. You have passed a string.");
     }
@@ -60,7 +62,7 @@ function multiRollDiceArrayObject(n = 2, m = 6) {
 
 //The output of the rice will be available as single elments of an array. 
 //i.e Each element of the array will be the number obtained from rolling the dice
-console.log(multiRollDiceArrayObject()); //<<== the function to get diceArray with objects as single elements.
+//console.log(multiRollDiceArrayObject()); //<<== the function to get diceArray with objects as single elements.
 
 
 
